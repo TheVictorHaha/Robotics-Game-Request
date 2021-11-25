d680f8334e009d247725b98ce18a2609d0df7cab
Parmesan-Cheez <victoryan001@gmail.com>
2021-11-25 10:39:12 -0700

    bullets kms outside camera

--- Robotics Gaming/Assets/PacBullet.cs
+++ Robotics Gaming/Assets/PacBullet.cs
@@ -9,10 +9,13 @@ public class PacBullet : MonoBehaviour
 
     private bool oriented;
     private Vector3 dir;
+
+    public GameObject cam;
     // Start is called before the first frame update
     void Start()
     {
         oriented = false;
+        cam = Camera.main.gameObject;
     }
 
     // Update is called once per frame
@@ -28,6 +31,19 @@ public class PacBullet : MonoBehaviour
             oriented = true;
         }
         transform.Translate(dir.normalized * speed * Time.deltaTime);
+
+        var upperLeftScreen = new Vector3(0, Screen.height);
+        var upperRightScreen = new Vector3(Screen.width, Screen.height);
+        var lowerLeftScreen = new Vector3(0, 0);
+        var lowerRightScreen = new Vector3(Screen.width, 0);
+
+        //Corner locations in world coordinates
+        var upperLeft = cam.GetComponent<Camera>().ScreenToWorldPoint(upperLeftScreen);
+        var upperRight = cam.GetComponent<Camera>().ScreenToWorldPoint(upperRightScreen);
+        var lowerLeft = cam.GetComponent<Camera>().ScreenToWorldPoint(lowerLeftScreen);
+        var lowerRight = cam.GetComponent<Camera>().ScreenToWorldPoint(lowerRightScreen);
+        if (transform.position.y > upperRight.y || transform.position.y < lowerLeft.y) Destroy(gameObject);
+        if (transform.position.x > upperRight.x || transform.position.x < upperLeft.x) Destroy(gameObject);
     }
 
     public void setPoint(Vector3 pt)
